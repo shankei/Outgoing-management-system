0b30a47c57eea7d77ddb4e0034324f78fd31657e
Shashank Agarwal <53314764+shankei@users.noreply.github.com>
2020-05-24 16:26:59 +0530

    Update server2.js

--- server2.js
+++ server2.js
@@ -670,7 +670,7 @@ const logger = winston.createLogger({
     // - Write all logs with level `error` and below to `error.log`
     // - Write all logs with level `info` and below to `combined.log`
     new winston.transports.File({ filename: 'combined.log' }),
-    new Elasticsearch(esTransportOpts)
+    //new Elasticsearch(esTransportOpts)
   ]
 });
 
